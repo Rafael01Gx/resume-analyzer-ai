3df6674e9cf4db4e9139abb16e799e066b3a7260
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-07-19 13:59:28 -0300

    Initial commit - setup inicial do projeto com TailwindCSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent,
+  }
+];
