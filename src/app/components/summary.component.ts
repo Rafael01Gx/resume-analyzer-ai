211c7b05b105e8d1ad70cf0b31fd9db7bfab2525
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-07-21 02:46:57 -0300

    feat: adiciona componente Summary com visualização geral da pontuação
    - Criado componente Summary com layout responsivo e estilização customizada
    - Integrado ScoreGauge para exibição visual da pontuação geral
    - Adicionados componentes Category para exibir scores individuais por área
    - Utilizado input reativo para receber dados de feedback dinamicamente
    - Estrutura preparada para visualização clara e orientada ao usuário

--- src/app/components/summary.component.ts
+++ src/app/components/summary.component.ts
@@ -1,9 +1,30 @@
 import {Component, input} from '@angular/core';
+import {ScoreGaugeComponent} from './score-gauge.component';
+import {CategoryComponent} from './category.component';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
-  template: ``
+  imports: [
+    ScoreGaugeComponent,CategoryComponent
+  ],
+  template: `
+    <div class="bg-white rounded-2xl shadow-md w-full">
+      <div class="flex flex-row items-center p-4 gap-8">
+        <app-score-gauge [score]="feedBack()?.overallScore || 0"/>
+        <div class="flex flex-col gap-2">
+          <h2 class="text-2xl font-bold">Sua pontuação de currículo</h2>
+          <p class="text-sm text-gray-500">
+            Esta pontuação é calculada com base nas variáveis listadas abaixo.
+          </p>
+        </div>
+        </div>
+
+      <app-category title="Tom e estilo" [score]="feedBack()?.toneAndStyle?.score || 0" />
+      <app-category title="Conteúdo" [score]="feedBack()?.content?.score || 0" />
+      <app-category title="Estrutura" [score]="feedBack()?.structure?.score || 0" />
+      <app-category title="Skills" [score]="feedBack()?.skills?.score || 0" />
+      </div>
+  `
 })
 export class SummaryComponent {
   feedBack = input<Feedback>()
