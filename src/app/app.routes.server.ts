ad9feb6c0f969a61522cab67c6681823d5041ec0
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-07-24 20:39:16 -0300

    Revert "deploy"
    This reverts commit 3a0eefc47cc5a74ea9cf4bd44df0e6ba02be2726.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: 'resume/:id',
     renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
   }
 ];
